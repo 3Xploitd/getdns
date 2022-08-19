1cc8077da7c05f82a4dd329ec6d47a10e616eb9a
3Xploitd <40121116+3Xploitd@users.noreply.github.com>
2022-08-19 01:14:32 -0500

    Update getdns.py

--- getdns.py
+++ getdns.py
@@ -2,9 +2,9 @@
 import dns.resolver
 import sys
 import re
+import requests
 
 def getdns(domain):
-
 
     dns_records = {}
     record_types = ['a','aaaa','mx','soa','ptr','cname','ns','txt','srv','info','caa']
@@ -34,5 +34,41 @@ def getdns(domain):
 
         print(each.upper(),'--> ',dns_data)
 
-getdns(sys.argv[1])
+
+def getsuddomains(domain):
+    subdomains = requests.get('https://crt.sh/?Identity={}'.format(domain)).text
+    subdomain_list = re.findall(r'[\w\-_\.\*]+?\.{}'.format(domain),subdomains)
+    subdomains_final = set()
+    for each in subdomain_list:
+        if each.startswith('*') == True:
+            each = each[2:]
+        subdomains_final.add(each)
+   # loop = True
+   # results=0
+   # while loop:
+   #     try:
+   #         google_search = requests.get('https://google.com/?q=site:'+domain+'&start='+str(results)).text
+   #         results+=10
+   #         subdomain_list = re.findall(r'[\w\-_\.\*]+?\.{}'.format(domain),google_search)
+   #         for each in subdomain_list:
+
+   #             subdomains_final.add(each)
+   #         print(len(subdomains_finals))
+   #         for each in subdomains_final:
+   #             print(each)
+   #     except:
+   #         loop = False
+   #         break
+
+    for each in sorted(subdomains_final):
+        print(each)
+
+    print("\n","{} unique subdomains were discovered for {}".format(len(subdomains_final),domain))
+if sys.argv[1] == '-d' or 'dns':
+    getdns(sys.argv[2])
+elif sys.argv[1] == '-s' or 'subdomains':
+    getsuddomains(sys.argv[2])
+
+else:
+    print("SyntaxError: Must specifiy which tool to use (ex. python3 getdns.py -d DOMAIN or python3 getdns.py -s DOMAIN)")
 sys.exit()
